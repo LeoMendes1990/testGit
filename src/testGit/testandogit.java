91e1007b58b71dd1b310a6290c9745344743b3e9
leo <leozitoo1990@gmail.com>
2023-10-14 18:39:37 -0300

    add construtor

--- src/testGit/testandogit.java
+++ src/testGit/testandogit.java
@@ -4,5 +4,15 @@ public class testandogit {
 	
 	String nome;
 	int numero;
+	public testandogit() {
+		super();
+	}
+	public testandogit(String nome, int numero) {
+		super();
+		this.nome = nome;
+		this.numero = numero;
+	}
+	
+	
 
 }
